a6b6906bcd162c9ecb8fc0382e1510038562adde
ali-mansouri-b <ali.mansouribot@gmail.com>
2025-04-08 19:51:23 +0330

    feat: use express routers

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var app = express();
 
+var things = require('./things.js');
+
 app.get('/hello', function(req, res){
    res.send("Hello World!");
 });
 
-app.post('/hello', function(req, res){
-   res.send("You just called the post method at '/hello'!\n");
-});
-
-app.all('/test', function(req, res){
-   res.send("HTTP method doesn't have any effect on this route!");
-}); 
+//both index.js and things.js should be in same directory
+app.use('/things', things);
 
 app.listen(3000);
