8524d969ef516a58189f237e59c8464e22685aee
ali-mansouri-b <ali.mansouribot@gmail.com>
2025-04-11 20:50:36 +0330

    feat: Pass Values to Templates

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-app.get('/first_template', function(req, res){
-   res.render('first_view');
+app.get('/dynamic_view', function(req, res){
+   res.render('dynamic', {
+      name: "TutorialsPoint", 
+      url:"http://www.tutorialspoint.com"
+   });
 });
 
 app.listen(3000);
