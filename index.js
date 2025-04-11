9ca2a93039c89c37db6df3b0ffadd28a2a2cfe2e
ali-mansouri-b <ali.mansouribot@gmail.com>
2025-04-11 20:55:16 +0330

    feat: use conditional variable

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ app.set('view engine', 'pug');
 app.set('views','./views');
 
 app.get('/dynamic_view', function(req, res){
-   res.render('dynamic', {
-      name: "TutorialsPoint", 
-      url:"http://www.tutorialspoint.com"
+   res.render('dynamic',{
+      user: {name: "Ayush", age: "20"}
    });
 });
+
 
 app.listen(3000);
